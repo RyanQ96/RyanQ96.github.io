dd4bb052c514ec823215ee87cd6cac0075627bee
Rui Qiu <ruiq@wustl.edu>
2025-04-23 14:00:52 -0400

    update

--- app.vue
+++ app.vue
@@ -17,8 +17,8 @@ import NavBarComp from './components/NavBarComp.vue'
 useSeoMeta({
   title: "Rui Qiu @ Ohio State University",
   ogTitle: 'Rui Qiu @ Ohio State University',
-  description: "Hi, this is Rui. I'm a Ph. D. student in Ohio State University. My research interests are broad range of Visual Analytics Science and Technologies (VAST). I'm documenting my learning notes and open source projects here in this blog. Besides, I'm a huge fan of VueJS, Vite and Observablehq.",
-  ogDescription: "Hi, this is Rui. I'm a Ph. D. student in Ohio State University. My research interests are broad range of Visual Analytics Science and Technologies (VAST). I'm documenting my learning notes and open source projects here in this blog. Besides, I'm a huge fan of VueJS, Vite and Observablehq.",
+  description: "Hi, this is Rui (Ray). I'm a Ph. D. student in Ohio State University. My research interests are broad range of Visual Analytics Science and Technologies (VAST). I'm documenting my learning notes and open source projects here in this blog. Besides, I'm a huge fan of VueJS, Vite and Observablehq.",
+  ogDescription: "Hi, this is Rui (Ray). I'm a Ph. D. student in Ohio State University. My research interests are broad range of Visual Analytics Science and Technologies (VAST). I'm documenting my learning notes and open source projects here in this blog. Besides, I'm a huge fan of VueJS, Vite and Observablehq.",
 })
 </script>
 
